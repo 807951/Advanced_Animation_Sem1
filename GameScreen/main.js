ed1f0599466e1218e4f6f5f7b5019ff78ffd0885
807951 <54371368+807951@users.noreply.github.com>
2020-09-18 11:53:56 -0700

    9/18 commit

--- GameScreen/main.js
+++ GameScreen/main.js
@@ -1,13 +1,12 @@
 // setup canvas
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
-//var width = canvas.width = window.innerWidth;
-//var height = canvas.height = window.innerHeight;
-// function to generate random number
-function random(min, max) {
-  var num = Math.floor(Math.random()*(max-min)) + min;
-  return num;
+window.onload = init;
+
+function init() {
+	canvas
 }
+
 function GameArea(){
     	//  Wrapper Div
     	this.wrapperDiv = document.getElementById("wrapperDiv");
@@ -19,69 +18,15 @@ function GameArea(){
 	// create tileDivs
 	this.tileDiv1 = document.createElement("div");
 	this.tileMenuDiv.appendChild(this.tileDiv1);
-	this.tileDiv1.setsetAttribute("style", " background-color:#FF0000; width:90px; height:90px;float:right;");
+	this.tileDiv1.setAttribute("style", " background-color:#FF0000; width:90px; height:90px;float:right;");
+
+	this.tileDiv2 = document.createElement("div");
+	this.tileMenuDiv.appendChild(this.tileDiv2);
+	this.tileDiv2.setAttribute("style", " background-color:#FF0000; width:90px; height:90px;float:right;");
+
+	this.tileDiv3 = document.createElement("div");
+	this.tileMenuDiv.appendChild(this.tileDiv3);
+	this.tileDiv3.setAttribute("style", " background-color:#FF0000; width:90px; height:90px;float:right;");
 }
 	
-// define Ball constructor
-function Ball() {
-  this.x = random(0,width);
-  this.y = random(0,height);
-  this.velX = random(-7,7);
-  this.velY = random(-7,7);
-  this.color = 'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')';
-  this.rad = random(10,20);
-}
-// define ball draw method
-Ball.prototype.draw = function() {
-  ctx.beginPath();
-  ctx.fillStyle = this.color;
-  ctx.arc(this.x, this.y, this.rad, 0, 2 * Math.PI);
-  ctx.fill();
-};
-// define ball update method
-Ball.prototype.update = function() {
-  if ((this.x + this.rad) > width) {
-    this.velX = -(this.velX);
-  } else if ((this.x - this.rad) < 0) {
-    this.velX = -(this.velX);
-  } else if ((this.y + this.rad) > height) {
-    this.velY = -(this.velY);
-  } else if ((this.y - this.rad) < 0) {
-    this.velY = -(this.velY);
-  }
-  this.x += this.velX;
-  this.y += this.velY;
-};
-// define ball collision detection
-Ball.prototype.collisionDetect = function() {
-  for(j = 0; j < balls.length; j++) {
-    if( (!(this.x === balls[j].x && this.y === balls[j].y && this.velX === balls[j].velX && this.velY === balls[j].velY)) ) {
-      var dx = this.x - balls[j].x;
-      var dy = this.y - balls[j].y;
-      var dist = Math.sqrt(dx * dx + dy * dy);
-
-      if (dist < this.rad + balls[j].rad) {
-        balls[j].color = this.color = 'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) +')';
-      }
-    }
-  }
-};
-// define array to store balls
-var balls = [];
-// define loop that keeps drawing the scene constantly
-function loop() {
-  ctx.fillStyle = 'rgba(0,0,0,0.25)';
-  ctx.fillRect(0,0,width,height);
-  while(balls.length < 20) {
-    var ball = new Ball();
-    balls.push(ball);
-  }
-  for(i = 0; i < balls.length; i++) {
-    balls[i].draw();
-    balls[i].update();
-    balls[i].collisionDetect();
-  }
-  requestAnimationFrame(loop);
-}
-//loop();
-GameArea();
+gameArea();
